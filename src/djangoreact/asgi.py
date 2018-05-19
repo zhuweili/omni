71baee976df4d0b96bc938b30361ffe962398faa
weilizhu <weilizhu@buffalo.edu>
2018-05-19 15:26:39 -0700

    second

--- src/djangoreact/asgi.py
+++
@@ -1,5 +0,0 @@
-import os
-import channels.asgi
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channels_example.settings")    #这里填的是你的配置文件settings.py的位置
-channel_layer = channels.asgi.get_channel_layer()
